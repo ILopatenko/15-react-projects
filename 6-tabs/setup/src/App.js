9ffe02c9d1814cb2e2466bbb30574cd0ba3182ca
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-12 12:40:52 -0800

    Project #6. Experience page with active classes buttons. TC02:13

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -1,10 +1,73 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
-function App() {
-  return <h2>tabs project setup</h2>
-}
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 
-export default App
+const url = 'https://course-api.com/react-tabs-project';
+
+const App = () => {
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const [jobsHistoryArray, setJobsHistoryArray] = useState([]);
+  const [jobForDisplay, setJobForDisplay] = useState(0);
+
+  const fetchDataFromAPI = async (linkToAPI) => {
+    const responseFromAPI = await fetch(linkToAPI);
+    const arrayOfJobsJson = await responseFromAPI.json();
+    setJobsHistoryArray(arrayOfJobsJson);
+    setIsDataLoaded(true);
+  };
+
+  useEffect(() => {
+    fetchDataFromAPI(url);
+  }, []);
+
+  const displayError = () => {
+    return <h1>Connectong to API ...</h1>;
+  };
+
+  const displayJob = () => {
+    const { id, order, title, dates, duties, company } = jobsHistoryArray[
+      jobForDisplay
+    ];
+    return (
+      <section className='section'>
+        <div className='title'>
+          <h2>Experience</h2>
+          <div className='underline'></div>
+        </div>
+        <div className='jobs-center'>
+          <div className='btn-container'>
+            {jobsHistoryArray.map((element, index) => {
+              return (
+                <button
+                  className={`job-btn ${
+                    index === jobForDisplay && 'active-btn'
+                  }`}
+                  key={element.id}
+                  onClick={() => setJobForDisplay(index)}
+                >
+                  {element.company}
+                </button>
+              );
+            })}
+          </div>
+          <article className='job-info'>
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className='job-date'>{dates}</p>
+            {duties.map((el, index) => {
+              return (
+                <div key={index} className='job-desc'>
+                  <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                  <p>{el}</p>
+                </div>
+              );
+            })}
+          </article>
+        </div>
+      </section>
+    );
+  };
+
+  return isDataLoaded ? displayJob() : displayError();
+};
+
+export default App;
