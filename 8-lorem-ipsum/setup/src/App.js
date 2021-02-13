78bf8ef400893229ed1fd666867ef2a34ce66d7c
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-12 16:25:05 -0800

    Project #9. Colors generator. TC03:51

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,42 @@
 import React, { useState } from 'react';
 import data from './data';
-function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
-}
 
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (amount <= 0) {
+      amount = 1;
+    } else if (amount > 5) {
+      amount = 5;
+    }
+    setText(data.slice(0, amount));
+  };
+  return (
+    <section className='section-center'>
+      <h3> Need some text? </h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>Paragpaphs: </label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((element, index) => {
+          return <p key={index}>{element}</p>;
+        })}
+      </article>
+    </section>
+  );
+};
 export default App;
--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -1,10 +1,54 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+import Values from 'values.js';
 
-import Values from 'values.js'
+const App = () => {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#f15025').all(5));
 
-function App() {
-  return <h2>color generator project</h2>
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(5);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
 
-export default App
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            generate
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((element, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...element}
+              index={index}
+              hexColor={element.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+export default App;
