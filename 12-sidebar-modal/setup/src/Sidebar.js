4ef998940d60bcf3a4225b85d844a2a49af93ecf
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-14 13:18:33 -0800

    Project #12. Sidebar and Modal (GLOBAL CONTEXT and CUSTOM HOOK). TC05:45

--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -1,10 +1,44 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from 'react';
+import logo from './logo.svg';
+import { FaTimes } from 'react-icons/fa';
+import { social, links } from './data';
+import { useMyCustommHook } from './context';
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  const { isSidebarOpen, closeSidebar } = useMyCustommHook();
+  return (
+    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
+      <div className='sidebar-header'>
+        <img src={logo} alt='LoGo' className='logo' />
+        <button className='close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((eachLink) => {
+          const { id, text, url, icon } = eachLink;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className='social-icons'>
+        {social.map((eachSocialLink) => {
+          const { id, url, icon } = eachSocialLink;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
